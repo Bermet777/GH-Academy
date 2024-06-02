136f750ab8768f46d1341126b501b36cb7ed55d8
bermet <bermet.alibek@gmail.com>
2024-06-02 14:59:23 -0500

    read and printed the file

--- text_compression/text_compress/src/App.java
+++ text_compression/text_compress/src/App.java
@@ -1,5 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+  public static void main(String[] args) {
+    try {
+      File myFile = new File("C:\\Users\\berme\\OneDrive\\Desktop\\ACADEMY\\GH-Academy\\text_compression\\text_compress\\input.txt");
+      Scanner myReader = new Scanner(myFile);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println(("An error occured"));
+      e.printStackTrace();
     }
+  }
 }
