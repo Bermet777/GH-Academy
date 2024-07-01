e7f3dadd525cda07c698dcf70fe87f08506aea06
bermet <bermet.alibek@gmail.com>
2024-06-30 23:50:55 -0500

    defined compress and decompress methods

--- text_compression/text_compress/src/App.java
+++ text_compression/text_compress/src/App.java
@@ -7,15 +7,23 @@ import java.util.zip.GZIPOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.zip.GZIPInputStream;
+
 
 // read the file and saved the words in a separate file
 public class App {
   public static void main(String[] args) {
+
+    String inputFilePath = "C:\\Users\\berme\\OneDrive\\Desktop\\ACADEMY\\GH-Academy\\text_compression\\text_compress\\input.txt";
+    String outputFilePath = "C:\\Users\\berme\\OneDrive\\Desktop\\ACADEMY\\GH-Academy\\text_compression\\text_compress\\output.sc";
+    String compressedFilePath = "C:\\Users\\berme\\OneDrive\\Desktop\\ACADEMY\\GH-Academy\\text_compression\\text_compress\\output.sc.gz";
+    String decompressedFilePath = "C:\\Users\\berme\\OneDrive\\Desktop\\ACADEMY\\GH-Academy\\text_compression\\text_compress\\readable.txt";
+
     HashMap<String, Integer> wordMap = new HashMap<>();
     int wordCount = 1;
 
     try {
-      File myFile = new File("C:\\Users\\berme\\OneDrive\\Desktop\\ACADEMY\\GH-Academy\\text_compression\\text_compress\\input.txt");
+      File myFile = new File(inputFilePath);
       Scanner myReader = new Scanner(myFile);
       while (myReader.hasNextLine()) {
         String data = myReader.nextLine();
@@ -28,34 +36,20 @@ public class App {
       }
       myReader.close();
 
-      FileWriter myWriter = new FileWriter("C:\\Users\\berme\\OneDrive\\Desktop\\ACADEMY\\GH-Academy\\text_compression\\text_compress\\output.sc");
+      FileWriter myWriter = new FileWriter(outputFilePath);
       for (HashMap.Entry<String, Integer> entry : wordMap.entrySet()) {
         myWriter.write(entry.getKey() + "\t" + entry.getValue() + "\n");
       }
       myWriter.close();
 
+      //compressed file
+
       compressFile(outputFilePath, compressedFilePath);
       System.out.println("File compressed");
 
-      // Step 4: Decompress the file
+      // decompressed file
       decompressFile(compressedFilePath, decompressedFilePath);
       System.out.println("File decompressed");
-
-//Compressed file      
-
-     FileInputStream fis = new FileInputStream("C:\\Users\\berme\\OneDrive\\Desktop\\ACADEMY\\GH-Academy\\text_compression\\text_compress\\output.sc");
-      FileOutputStream fos = new FileOutputStream("C:\\Users\\berme\\OneDrive\\Desktop\\ACADEMY\\GH-Academy\\text_compression\\text_compress\\output.sc.gz");
-      GZIPOutputStream gzipOS = new GZIPOutputStream(fos);
-      byte[] buffer = new byte[1024];
-      int len;
-      while((len=fis.read(buffer)) != -1){
-        gzipOS.write(buffer, 0, len);
-      }
-      fis.close();
-      gzipOS.close();
-      fos.close();
-
-      System.out.println("File compressed");
 
     } catch (FileNotFoundException e) {
       System.out.println(("An error occurred"));
@@ -65,4 +59,33 @@ public class App {
       e.printStackTrace();
     }
   }
+// compress and decompress methods
+  private static void compressFile(String inputFilePath, String compressedFilePath) throws IOException {
+    FileInputStream fis = new FileInputStream(inputFilePath);
+    FileOutputStream fos = new FileOutputStream(compressedFilePath);
+    GZIPOutputStream gzipOS = new GZIPOutputStream(fos);
+    byte[] buffer = new byte[1024];
+    int len;
+    while ((len = fis.read(buffer)) != -1) {
+        gzipOS.write(buffer, 0, len);
+    }
+    fis.close();
+    gzipOS.close();
+    fos.close();
+}
+
+
+private static void decompressFile(String compressedFilePath, String decompressedFilePath) throws IOException {
+  FileInputStream fis = new FileInputStream(compressedFilePath);
+  GZIPInputStream gzipIS = new GZIPInputStream(fis);
+  FileOutputStream fos = new FileOutputStream(decompressedFilePath);
+  byte[] buffer = new byte[1024];
+  int len;
+  while ((len = gzipIS.read(buffer)) != -1) {
+      fos.write(buffer, 0, len);
+  }
+  gzipIS.close();
+  fis.close();
+  fos.close();
+}
 }
